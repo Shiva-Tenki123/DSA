28d5f04222a5edf917925e386e0d8bbb0e99e9aa
Tenki Shiva <tenkishiva06@gmail.com>
2025-03-01 20:33:05 +0530

    Time: 4 ms (86.24%) | Memory: 44.9 MB (6.99%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] res = new int[nums.length];
+        int[] sortedNums = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sortedNums);
+        
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = Arrays.binarySearch(sortedNums, nums[i]);
+            if (res[i] < 0) {
+                res[i] = -res[i] - 1; // Get the insertion point
+            } else {
+                // If the number is found, count how many times it appears
+                while (res[i] > 0 && sortedNums[res[i] - 1] == nums[i]) {
+                    res[i]--;
+                }
+            }
+        }
+        
+        return res;
+    }
+}
