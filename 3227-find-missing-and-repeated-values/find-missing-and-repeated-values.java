f470eb750626edfd5a0145e1e0d4f332affc3ba8
Tenki Shiva <tenkishiva06@gmail.com>
2025-03-07 15:27:56 +0530

    Time: 1 ms (100.00%) | Memory: 45.5 MB (79.98%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+
+        long N = (long) n * n;
+
+        long gridSum = 0;
+        long gridSqSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                gridSum += grid[i][j];
+                gridSqSum += (long) grid[i][j] * grid[i][j];
+            }
+        }
+
+        long sum = (N * (N + 1)) / 2; 
+        long sqSum = (N * (N + 1) * (2 * N + 1)) / 6; 
+        long sqDiff = gridSqSum - sqSum;
+        long sumDiff = gridSum - sum;
+
+        int a = (int) ((sqDiff / sumDiff + sumDiff) / 2);
+        int b = (int) ((sqDiff / sumDiff - sumDiff) / 2);
+
+        return new int[]{a, b};
+    }
+}
